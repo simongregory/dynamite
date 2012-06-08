1b3de1b34009396062c776def70c4dab4330c336
Simon Gregory <simon@helvector.org>
2012-06-08 12:35:08 +0100

    go green with a hardcoded playlist epic and vpid

--- features/support/check.rb
+++ features/support/check.rb
@@ -9,7 +9,7 @@ def check(method,feed_name,results_format)
   when 'multi now next radio' then @response = method.multi_now_next('radio')
   when 'multi now next tv' then @response = method.multi_now_next('tv')
   when 'now next' then @response = method.now_next
-  when 'playlist' then @response = method.playlist('aaaaaaaa','bbbbbbbb')
+  when 'playlist' then @response = method.playlist('b01jc7xv', 'b01jc7rc')
 
   else
     fail "Didn't find a case for the provided feed name."
